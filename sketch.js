929ef4192540504d372f3c20b482bf8d133e768b
Sheema Parwaz <Sheemaparwaz@gmail.com>
2021-07-07 12:59:26 +0530

    debugged

--- sketch.js
+++ sketch.js
@@ -1,210 +1,257 @@
-var canvas ;
-var groundImg, lotousImg , rainbowImg , unicornImg, ground2Img,coneImg,fenceImg,fireimg,lightningImg,bladeImg,stoneimg,bronzeImg,goldImg,silverimg,diamondImg;
-var ground, lotous, rainbow , unicorn, ground2, ground3,cone,fence,fire,lightning,blade,stone,bronze,gold,silver,diamond;
+var canvas;
+
+var rainbow, unicorn;
+var rainbowImg, unicornImg;
+
+var ground, ground2, ground3, ground4, invisibleGround, ground3X, ground4X;
+var groundImg, ground2Img, ground3Img, ground4Img;
+
+var cone, fence, fire, lightning, blade, stone;
+var coneImg, fenceImg, fireImg, lightningImg, bladeImg, stoneImg;
+
+var bronze, gold, silver, diamond;
+var bronzeImg, goldImg, silverImg, diamondImg;
 
 
+function preload() {
+  groundImg = loadImage('images/gr.png');
+  ground2Img = loadImage('images/gr2.png');
+  ground3Img = loadImage('images/gr3.png');
+  ground4Img = loadImage('images/gr4.png')
 
+  rainbowImg = loadImage('images/bg.png');
+  unicornImg = loadAnimation(
+    'images/unicorn/uni1.png',
+    'images/unicorn/uni2.png',
+    'images/unicorn/uni3.png',
+    'images/unicorn/uni4.png',
+    'images/unicorn/uni5.png',
+    'images/unicorn/uni6.png',
+    'images/unicorn/uni7.png'
+  );
 
+  goldImg = loadImage('images/rewards/gold.png');
+  bronzeImg = loadImage('images/rewards/bronze.png');
+  silverImg = loadImage('images/rewards/silver.png');
+  diamondImg = loadImage('images/rewards/diamond.png');
 
-
-function preload (){
-  groundImg = loadImage("images/gr2.png");
-
-  lotousImg = loadImage("images/lotus.png");
-  rainbowImg = loadImage("images/bg.png");
-  unicornImg = loadAnimation("images/unicorn/uni1.png","images/unicorn/uni2.png","images/unicorn/uni3.png","images/unicorn/uni4.png","images/unicorn/uni5.png","images/unicorn/uni6.png","images/unicorn/uni7.png");
-  ground2Img = loadImage("images/gr.png");
-  ground3img = loadImage ("images/gr3.png");
-
-  goldImg = loadImage("images/rewards/gold.png");
-  bronzeImg = loadImage("images/rewards/bronze.png");
-  silverImg = loadImage("images/rewards/silver.png");
-  diamondImg = loadImage("images/rewards/diamond.png");
-
-  fireImg = loadImage ("images/obstacles/fire.png")
-  lightningImg = loadImage ("images/obstacles/lightning.png")
-  fenceImg = loadImage ("images/obstacles/fence.png")
-  ninjaBladeImg = loadImage ("images/obstacles/ninjaBlade.png")
-  stoneImg = loadImage ("images/obstacles/stone.png")
-  coneImg = loadImage ("images/obstacles/cone.png")
+  fireImg = loadImage('images/obstacles/fire.png');
+  lightningImg = loadImage('images/obstacles/lightning.png');
+  fenceImg = loadImage('images/obstacles/fence.png');
+  ninjaBladeImg = loadImage('images/obstacles/ninjaBlade.png');
+  stoneImg = loadImage('images/obstacles/stone.png');
+  coneImg = loadImage('images/obstacles/cone.png');
 }
 
-
-
-
-
 function setup() {
-    canvas = createCanvas (displayWidth-30,displayHeight-95)
+  canvas = createCanvas(windowWidth, windowHeight);
 
-    ground = createSprite ((displayWidth/2)-500,displayHeight-270);
-    ground.addImage (ground2Img);
-    ground.scale = 0.2;
-    ground.velocityX = -2;
+  invisibleGround = createSprite(windowWidth/2, windowHeight-20, windowWidth, 20);
+  invisibleGround.visible = false;
 
-    ground2 = createSprite ((displayWidth/2)+250,displayHeight-300);
-    ground2.addImage (groundImg);
-    ground2.scale = 1.7;
-    ground2.velocityX = -2;
+  ground = createSprite(440, windowHeight - 176);
+  ground.addImage(ground2Img);
+  ground.scale = 1.7;
+  ground.velocityX = -2;
+  ground.debug = false;
 
-    ground3 = createSprite (displayWidth,displayHeight-190);
-    ground3.addImage (ground3img);
-    ground3.velocityX= -2;
+  ground2 = createSprite(windowWidth / 2 + 510, windowHeight - 176);
+  ground2.addImage(ground2Img);
+  ground2.scale = 1.7;
+  ground2.velocityX = -2;
+  ground2.debug = false;
 
-    rainbow = createSprite (displayWidth/2,160);
-    rainbow.addImage (rainbowImg)
+  ground3 = createSprite(windowWidth+580, windowHeight - 176);
+  ground3.addImage(ground2Img);
+  ground3.scale = 1.7;
+  ground3.velocityX = -2;
+  ground3.debug = false;
+  ground3X = ground3.x
 
-    unicorn = createSprite (displayWidth/2,displayHeight-315,displayWidth,300);
-    unicorn.addAnimation("running",unicornImg)
-    unicorn.scale=2.5;
-    unicorn.velocityX = 1;
+  rainbow = createSprite(windowWidth / 2, 160);
+  rainbow.addImage(rainbowImg);
 
-
-
+  unicorn = createSprite(200,windowHeight - 130);
+  unicorn.addAnimation('running', unicornImg);
+  unicorn.scale = 2;
+  unicorn.debug = false;
+  unicorn.setCollider("rectangle",0,0,200,110);
+  //unicorn.velocityX = 1;
 }
 
 function draw() {
-  background("lightblue");
-  if (ground.x <-150) {
-  ground.x = displayWidth+250;
+  background('lightblue');
+
+  
+
+ if (ground.x < - 450) {
+    ground.x = ground3X-20;
   }
 
-  if (ground2.x <0) {
-    ground2.x = displayWidth+100;
+  if (ground2.x < - 450) {
+    ground2.x = ground3X-20;
+  }
+
+  if (ground3.x < - 450) {
+    ground3.x = ground3X-20;
+  }
+
+  spawnObstacles();
+  spawnRewards();
+
+  unicorn.collide(invisibleGround);
+
+  drawSprites();
+}
+
+function spawnObstacles() {
+  if (frameCount % 200 === 0) {
+    obstacle = createSprite(windowWidth, windowHeight - 350);
+    obstacle.velocityX = -5;
+    var rand = Math.round(random(1, 6));
+
+    switch (rand) {
+      case 1:
+        {
+          obstacle.addImage(coneImg);
+          obstacle.scale = 0.5;
+          obstacle.y = windowHeight - 200;
+        }
+        break;
+      case 2:
+        {
+          obstacle.addImage(fenceImg);
+          obstacle.scale = 1;
+          obstacle.y = windowHeight - 200;
+        }
+        break;
+      case 3:
+        {
+          obstacle.addImage(fireImg);
+          obstacle.scale = 0.2;
+          obstacle.y = windowHeight - 200;
+        }
+        break;
+      case 4:
+        {
+          obstacle.addImage(lightningImg);
+          obstacle.scale = 1;
+          obstacle.y = windowHeight - 350;
+        }
+        break;
+      case 5:
+        {
+          obstacle.addImage(ninjaBladeImg);
+          obstacle.scale = 0.4;
+          obstacle.y = windowHeight - 350;
+        }
+        break;
+      case 6:
+        {
+          obstacle.addImage(stoneImg);
+          obstacle.scale = 0.5;
+          obstacle.y = windowHeight - 200;
+        }
+        break;
+
+      default:
+        console.log('no obstacle');
+        break;
     }
-camera.position.x = unicorn.x;
+    obstacle.lifetime = windowHeight / 5;
 
-
-spawnObstacles ();
-spawnRewards ();
-
-drawSprites();
-
-}
-
-function spawnObstacles () {
-  if (frameCount%200===0) {
-    obstacle = createSprite(displayWidth,displayHeight-350);
-    obstacle.velocityX=-5;
-    var rand = Math.round (random(1,6));
-
-  switch (rand){
-    case 1 : {obstacle.addImage (coneImg)
-   obstacle.scale = 1 ;
-   obstacle.y = displayHeight - 200
-  }
-   break;
-   case 2 : {obstacle.addImage (fenceImg)
-    obstacle.scale = 1;
-    obstacle.y = displayHeight - 200
-   }
-   break;
-   case 3 : {obstacle.addImage (fireImg)
-    obstacle.scale = 0.5
-    obstacle.y = displayHeight - 200
-    }
-   break;
-   case 4 : {obstacle.addImage (lightningImg)
-    obstacle.scale = 1;
-    obstacle.y = displayHeight - 350
-  }
-   break;
-   case 5 : {obstacle.addImage (ninjaBladeImg)
-   obstacle.scale = 0.4;
-   obstacle.y = displayHeight - 350
-}
-   break;
-   case 6 : {obstacle.addImage (stoneImg)
-    obstacle.scale = 0.5;
-    obstacle.y = displayHeight - 200
-  }
-   break;
-
-
-  default: console.log("no obstacle")
-  break;
-}
-    obstacle.lifetime=displayHeight/5;
-    //obstacle.scale=0.18;
     //obstacle.debug=true;
     //obstacle.setCollider("rectangle",0,0,350,350);
     //obstacleGroup.add(obstacle);
-
   }
-
-
 }
 
+function spawnRewards() {
+  if (frameCount % 500 === 0) {
+    reward = createSprite(windowWidth, windowHeight - 350);
+    reward.velocityX = -5;
+    var randN = Math.round(random(1, 10));
 
-function spawnRewards (){
-  if (frameCount%500===0) {
-reward = createSprite(displayWidth,displayHeight-350);
-reward.velocityX=-5;
-    var randN= Math.round (random(1,10));
-
-    switch (randN){
-      case 1 : {reward.addImage (bronzeImg)
-     reward.scale = 0.5 ;
-     reward.y = displayHeight - 200
-     }
-      break;
-
-      case 2 : {reward.addImage (bronzeImg)
-        reward.scale = 0.5;
-        reward.y = displayHeight - 200
+    switch (randN) {
+      case 1:
+        {
+          reward.addImage(bronzeImg);
+          reward.scale = 0.5;
+          reward.y = windowHeight - 200;
         }
-         break;
+        break;
 
-       case 3 : {reward.addImage (bronzeImg)
-          reward.scale = 0.5 ;
-          reward.y = displayHeight - 200
-          }
-           break;
+      case 2:
+        {
+          reward.addImage(bronzeImg);
+          reward.scale = 0.5;
+          reward.y = windowHeight - 200;
+        }
+        break;
 
-        case 4 : {reward.addImage (bronzeImg)
-            reward.scale = 0.5;
-            reward.y = displayHeight - 200
-            }
-             break;
+      case 3:
+        {
+          reward.addImage(bronzeImg);
+          reward.scale = 0.5;
+          reward.y = windowHeight - 200;
+        }
+        break;
 
-        case 5 : {reward.addImage (silverImg)
-              reward.scale = 0.5;
-              reward.y = displayHeight - 200
-              }
-               break;
+      case 4:
+        {
+          reward.addImage(bronzeImg);
+          reward.scale = 0.5;
+          reward.y = windowHeight - 200;
+        }
+        break;
 
-        case 6 : {reward.addImage (silverImg)
-                reward.scale = 0.5 ;
-                reward.y = displayHeight - 200
-                }
-                break;
+      case 5:
+        {
+          reward.addImage(silverImg);
+          reward.scale = 0.5;
+          reward.y = windowHeight - 200;
+        }
+        break;
 
-          case 7 : {reward.addImage (silverImg)
-                  reward.scale = 0.5;
-                  reward.y = displayHeight - 200
-                  }
-          break;
+      case 6:
+        {
+          reward.addImage(silverImg);
+          reward.scale = 0.5;
+          reward.y = windowHeight - 200;
+        }
+        break;
 
-                      case 8 : {reward.addImage (goldImg)
-                    reward.scale = 0.5 ;
-                    reward.y = displayHeight - 200
-                    }
-              break;
+      case 7:
+        {
+          reward.addImage(silverImg);
+          reward.scale = 0.5;
+          reward.y = windowHeight - 200;
+        }
+        break;
 
-              case 9 : {reward.addImage (goldImg)
-                      reward.scale = 0.5 ;
-                      reward.y = displayHeight - 200
-                      }
-                break;
+      case 8:
+        {
+          reward.addImage(goldImg);
+          reward.scale = 0.5;
+          reward.y = windowHeight - 200;
+        }
+        break;
 
-            case 10: {reward.addImage (diamondImg)
-              reward.scale = 0.5 ;
-                        reward.y = displayHeight - 200
-                        }
-            break;
+      case 9:
+        {
+          reward.addImage(goldImg);
+          reward.scale = 0.5;
+          reward.y = windowHeight - 200;
+        }
+        break;
 
-}
-
-
-
+      case 10:
+        {
+          reward.addImage(diamondImg);
+          reward.scale = 0.5;
+          reward.y = windowHeight - 200;
+        }
+        break;
+    }
   }
 }
